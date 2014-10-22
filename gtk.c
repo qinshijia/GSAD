0667655164d47884ae4d6552e23c97ffbe486b92
qinshijia <renxinyuanweb@163.com>
2014-10-22 19:29:10 +0800

    完善图形界面的跳转

--- gtk.c
+++ gtk.c
@@ -1,25 +1,6 @@
 #include <gtk/gtk.h>
 #include <string.h>
 #include "mytu.h"
-/*
-void button_clicked_dfs(GtkWidget *widget,gpointer data)
-{
-//gtk_main_quit ();  //退出程序
-	GtkWidget *btn;  
-	GtkBuilder *builder;
-	GtkWidget *window;
-	builder = gtk_builder_new ();//指針分配空間
-	gtk_builder_add_from_file(builder,"DFS.glade",NULL);
-	window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));//獲取window串口使用權
-	btn = GTK_WIDGET (gtk_builder_get_object (builder, "all"));//獲取控件使用權
-	gtk_builder_connect_signals (builder, NULL);//連接裏面的信號到槽
-	gtk_container_add(GTK_CONTAINER(window), btn);
-  //      g_signal_connect(G_OBJECT(btn),"clicked",G_CALLBACK(button_clicked_cb),NULL);
-	g_object_unref (G_OBJECT (builder));  //釋放xml內存空間
-	gtk_widget_show (window);//顯示窗體
-	gtk_main ();//迴路等待
-}
-*/
 
 GtkWidget *mainwindow;
 GtkWidget *Tstart,*Tend; 
@@ -30,21 +11,19 @@ GtkWidget *Ffile;
 void BFS_step(GtkWidget *widget,gpointer data){
 	ALGraph G;
 	char find = 'e';
-//	char fileName[27]; 
-//	CreatALGraph(&G,fileName);
-//	show(&G);
-//	BFS(&G,find);
   	const	char *start,*end;
 	const 	char *file;
 	start = gtk_entry_get_text(GTK_ENTRY(Tstart));
 	end = gtk_entry_get_text(GTK_ENTRY(Tend));
 	file= gtk_file_chooser_get_filename (GTK_FILE_CHOOSER(Ffile));
-//	fileName =(char *) file;
+
+	printf("BFS_step\tstart %s\t end %s\tfilename %s len %d\n",start,end,file,strlen(file));
+	if(file == NULL){
+		gtk_message_dialog_new(gtk_widget_get_parent_window(Ffile),GTK_DIALOG_MODAL,GTK_MESSAGE_ERROR,GTK_BUTTONS_OK,NULL);
+	}
 	CreatALGraph(&G,file);
 	show(&G);
 	BFS(&G,*end);
-
-
 	printf("BFS_step\tstart %s\t end %s\tfilename %s\n",start,end,file);
 }
 void BFS_next(GtkWidget *widget,gpointer data){
@@ -57,15 +36,26 @@ void BFS_next(GtkWidget *widget,gpointer data){
 	DFS(&G,find);
 	printf("BFS_next\n");
 }
+
+//关闭算法窗口，返回主窗口
+void back_parent(GtkWidget *widget,gpointer data){
+	gtk_widget_show (mainwindow);//顯示窗體
+}
+void exit(GtkWidget *widget,gpointer data){
+	gtk_main_quit ();  //退出程序
+}
+
 void button_clicked_bfs(GtkWidget *widget,gpointer data)
 {
-//gtk_main_quit ();  //退出程序
+	gtk_widget_hide(mainwindow); //隐藏主窗口
+
 	GtkWidget *Bstep,*Bcont;  
 	GtkBuilder *builder;
-	GtkWidget *window;
+	GtkWidget *window,*parent;
 	builder = gtk_builder_new ();//指針分配空間
 	gtk_builder_add_from_file(builder,"BFS.glade",NULL);
 	window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));//獲取window串口使用權
+//	parent = gtk_widget_get_parent_window(parent);
 
 	Bstep = GTK_WIDGET (gtk_builder_get_object (builder, "Bstep"));//獲取控件使用權
 	Bcont = GTK_WIDGET (gtk_builder_get_object (builder, "Bcont"));//獲取控件使用權
@@ -81,6 +71,7 @@ void button_clicked_bfs(GtkWidget *widget,gpointer data)
 	filter = gtk_file_filter_new();
 	gtk_file_filter_add_pattern(filter,"*.txt");		//过滤 只能选择 .txt 文件
 	gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (Ffile), filter);
+	gtk_file_chooser_set_filename(GTK_FILE_CHOOSER(Ffile),"a.txt" );
 
 	gtk_builder_connect_signals (builder, NULL);//連接裏面的信號到槽
 	gtk_container_add(GTK_CONTAINER(window), Bstep);
@@ -88,9 +79,11 @@ void button_clicked_bfs(GtkWidget *widget,gpointer data)
         g_signal_connect(G_OBJECT(Bstep),"clicked",G_CALLBACK(BFS_step),NULL);
         g_signal_connect(G_OBJECT(Bcont),"clicked",G_CALLBACK(BFS_next),NULL);
 
+	g_signal_connect_swapped(G_OBJECT(window),"destroy",G_CALLBACK(back_parent),NULL );
 	g_object_unref (G_OBJECT (builder));  //釋放xml內存空間
 	gtk_widget_show (window);//顯示窗體
-	gtk_main ();//迴路等待
+//	gtk_main ();//迴路等待
+//	gtk_widget_show (mainwindow);//顯示窗體
 }
 
 int main (int argc, char *argv[])
@@ -101,6 +94,7 @@ int main (int argc, char *argv[])
 	builder = gtk_builder_new ();//指針分配空間
 	gtk_builder_add_from_file(builder,"main.glade",NULL);
 	mainwindow = GTK_WIDGET (gtk_builder_get_object (builder, "mainwindow"));//獲取window串口使用權
+	gtk_window_set_title(mainwindow,"图搜索算法对比研究");
 
 	Ball = GTK_WIDGET (gtk_builder_get_object (builder, "all"));	//獲取控件使用權
 	Bdfs = GTK_WIDGET (gtk_builder_get_object (builder, "DFS"));
@@ -113,6 +107,7 @@ int main (int argc, char *argv[])
 	gtk_builder_connect_signals (builder, NULL);//連接裏面的信號到槽
 	gtk_container_add(GTK_CONTAINER(mainwindow), Bbfs);
         g_signal_connect(G_OBJECT(Bbfs),"clicked",G_CALLBACK(button_clicked_bfs),NULL);
+	g_signal_connect_swapped(G_OBJECT(mainwindow),"destroy",G_CALLBACK(exit),NULL );
 	g_object_unref (G_OBJECT (builder));  //釋放xml內存空間
 	gtk_widget_show (mainwindow);//顯示窗體
 	gtk_main ();//迴路等待
